111d60f88bee2968854e40f8deb0fe0297dc8236
BeroVero <to.vero.to@gmail.com>
2024-04-25 18:20:54 +0200

    added Api call based on city input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+function callApiViaCity(city) {
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+}
+
 function displayCityName(event) {
   event.preventDefault();
   let cityFormInput = document.querySelector("#city-form-input");
-
   cityDisplayedApp = document.querySelector("#app-city");
   cityDisplayedApp.innerHTML = cityFormInput.value;
+
+  callApiViaCity(cityFormInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", displayCityName);
+
+let apiKey = "34f7boabet4bfa3ede2162049aca8d1a";
