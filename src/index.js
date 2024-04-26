c54e5cf13df6d2f54d3fa6340d561f233e442b06
BeroVero <to.vero.to@gmail.com>
2024-04-26 13:49:16 +0200

    Added Api integration to search functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+function updateWeather(response) {
+  let temperatureApi = response.data.temperature.current;
+  let temperatureLoc = document.querySelector("#app-temperature");
+  temperatureLoc.innerHTML = Math.round(temperatureApi);
+
+  cityDisplayedApp = document.querySelector("#app-city");
+  cityDisplayedApp.innerHTML = response.data.city;
+}
+
 function callApiViaCity(city) {
   apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function displayCityName(event) {
   event.preventDefault();
   let cityFormInput = document.querySelector("#city-form-input");
-  cityDisplayedApp = document.querySelector("#app-city");
-  cityDisplayedApp.innerHTML = cityFormInput.value;
 
   callApiViaCity(cityFormInput.value);
 }
@@ -15,3 +23,6 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", displayCityName);
 
 let apiKey = "34f7boabet4bfa3ede2162049aca8d1a";
+
+// standard value when you load page
+callApiViaCity("Amsterdam");
