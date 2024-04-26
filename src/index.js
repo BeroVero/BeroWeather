2beaec093be55df306d3d55f3b14c4d7c7219473
BeroVero <to.vero.to@gmail.com>
2024-04-26 14:44:39 +0200

    More API integration (e.g humidity)+ date

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
+function formatDate(date) {
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let timeDisplayed = document.querySelector("#time-app");
+  let weekDayName = weekDays[date.getDay()];
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  timeDisplayed.innerHTML = `${weekDayName}, ${hours}:${minutes}`;
+}
+
 function updateWeather(response) {
   let temperatureApi = response.data.temperature.current;
   let temperatureLoc = document.querySelector("#app-temperature");
   temperatureLoc.innerHTML = Math.round(temperatureApi);
 
-  cityDisplayedApp = document.querySelector("#app-city");
-  cityDisplayedApp.innerHTML = response.data.city;
+  let cityDisplayed = document.querySelector("#app-city");
+  cityDisplayed.innerHTML = response.data.city;
+
+  let conditionDisplayed = document.querySelector("#app-condition");
+  conditionDisplayed.innerHTML = response.data.condition.description;
+
+  let humidityDisplayed = document.querySelector("#humidity-app");
+  humidityDisplayed.innerHTML = response.data.temperature.humidity;
+
+  let windDisplayed = document.querySelector("#wind-app");
+  windDisplayed.innerHTML = response.data.wind.speed;
+  let date = new Date(response.data.time * 1000);
+
+  formatDate(date);
 }
 
 function callApiViaCity(city) {
